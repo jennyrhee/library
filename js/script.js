eb38c26459adb3f71478fd87e0160f6725a10636
jennyrhee <jennyirhee@gmail.com>
2022-02-04 16:43:48 -0600

    Add makeCard function
    * Basic CSS for card-container and card
    
    * Loop to create cards for books in library array

--- js/script.js
+++ js/script.js
@@ -4,8 +4,24 @@ let library = [
 ];
 
 function Book(title, author, pages, hasRead) {
-  this.title = title;
+  this.title = `"${title}"`;
   this.author = author;
-  this.pages = pages;
+  this.pages = pages + ' pages';
   this.hasRead = hasRead;
 }
+
+function makeCard(book) {
+  const container = document.querySelector('.card-container');
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  for (let prop in book) {
+    let div = document.createElement('div');
+    div.classList.add(prop);
+    div.textContent = book[prop];
+    card.append(div);
+  }
+  container.appendChild(card);
+}
+
+library.forEach(book => makeCard(book));
